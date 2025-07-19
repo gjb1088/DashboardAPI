a852dfece0ce5592edd42daa2c04bc2b1c666847
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-19 15:14:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import svelteConfig from './svelte.config.js';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte({
+      // merge in the preprocess step
+      preprocess: svelteConfig.preprocess
+    })
+  ]
 });
