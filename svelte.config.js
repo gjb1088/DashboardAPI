614185ffb298129cd1f06ffae9ef475e70109359
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 10:51:46 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,1 +1,25 @@
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Use the same tsconfig settings
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: adapter({
+      // these folders are relative to project root
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    // if hosting at a sub-path, set base here
+    paths: {
+      base: ''
+    }
+  }
+};
+
+export default config;
 
