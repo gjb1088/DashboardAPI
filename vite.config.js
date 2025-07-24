52e51997c084c213311b56c651fc63152f10a591
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 10:24:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+// vite.config.js
 import { defineConfig } from 'vite';
+import { svelte }        from '@sveltejs/vite-plugin-svelte';
+import { resolve }       from 'path';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'src/app.html')
+    }
+  }
 });
