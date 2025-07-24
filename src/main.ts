23d1171d9feeefeebdfcbf39454cfdcee6fc7ba3
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-23 21:20:15 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-// dashboard/src/main.ts
 import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app'),
+new App({
+  target: document.getElementById('app')
 });
-
-export default app;
