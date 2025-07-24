dd5346478e12e21d9c5bf61bdb4920066c4c31c2
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 07:55:22 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({ pages: 'build', assets: 'build', fallback: null }),
     paths: { base: '' }
