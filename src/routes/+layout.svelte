344179b24069f2a8c0e5868e0a364b1314cc90cd
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-23 20:05:32 -0400

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let data;
+</script>
+
+<slot />
+
+<style global>
+  body {
+    margin: 0;
+    font-family: 'Press Start 2P', monospace;
+    background: linear-gradient(
+      135deg,
+      #110022 0%,
+      #001144 50%,
+      #220022 100%
+    );
+    color: #fff;
+  }
+  /* remove default button styles everywhere */
+  button {
+    font-family: inherit;
+  }
+</style>
