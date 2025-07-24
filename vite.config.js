cb84fe58841b5acb0510ac55047d4c854bbd9cef
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 09:21:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    // if you need CORS or proxies, configure them here
-  }
+  plugins: [svelte()]
 });
