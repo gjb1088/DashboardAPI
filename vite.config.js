3368ac74557c39bae06196647d97f72b1db5f9d6
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 13:45:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte }       from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  server: { port: 3000 }
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  }
 });
