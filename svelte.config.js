7d487de4533ab96878607a9148dc205396a5f012
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 13:51:35 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-// plain Vite + Svelte needs this so `<script lang="ts">` works
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('svelte').Config} */
 export default {
   preprocess: preprocess({
-    typescript: {
-      // only strip TS – don’t type-check
-      transpileOnly: true
-    }
+    // Only strip TS syntax—don’t enforce types
+    typescript: { transpileOnly: true }
   })
 };
