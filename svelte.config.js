938579b8876753c2621a3c2089ab5eb2beb22eae
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-24 08:43:11 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // enable TypeScript, SCSS, PostCSS, etc.
   preprocess: vitePreprocess(),
-
   kit: {
-    // build into the "build" folder (matches your Pages output setting)
     adapter: adapter({
-      pages: 'build',
+      pages:  'build',
       assets: 'build',
-      // for a SPA fallback, you could use: fallback: 'index.html'
       fallback: null
     }),
-
-    // no base path
     paths: {
       base: ''
     }
