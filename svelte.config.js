da1fe68541f25904c14513fcf730430091124346
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-23 20:12:07 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
 import preprocess from 'svelte-preprocess';
 
 export default {
   preprocess: preprocess({
-    typescript: {
-      // skip all type-checking and just compile
-      transpileOnly: true
-    }
-  }),
-
-  kit: {
-    adapter: adapter(),
-    // ...any other kit settings you had
-  }
+    typescript: { transpileOnly: true }
+  })
 };
