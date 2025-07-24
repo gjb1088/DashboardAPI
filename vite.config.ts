e563a105c3c9cb1795d10df63e69a365bb9a7a3f
gjb1088 <39758034+gjb1088@users.noreply.github.com>
2025-07-23 20:14:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
-import { svelte }         from '@sveltejs/vite-plugin-svelte';
-import preprocess         from 'svelte-preprocess';
+import { defineConfig }     from 'vite';
+import { svelte }           from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess     from 'svelte-preprocess';
 
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: preprocess({
+      preprocess: sveltePreprocess({
+        // ⚠️ skip all type-checking errors, just transpile TS → JS
         typescript: { transpileOnly: true }
       })
     })
